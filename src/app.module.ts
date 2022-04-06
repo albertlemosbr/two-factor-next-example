708a5e7d542da4b36a14234bcac8eb38d2f681cf
Albert Lemos <albert.bit8@gmail.com>
2022-04-06 16:50:18 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+
+@Module({
+  imports: [],
+  controllers: [AppController, AuthController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,40 @@
+import { Body, Controller, Get, Header, Post, Req } from '@nestjs/common';
+import { generateSecret, totp } from 'speakeasy';
+
+@Controller('auth')
+export class AuthController {
+  @Get('qr-code')
+  @Header('content-type', 'text/html')
+  async getQRCode(@Req() req): Promise<any> {
+    try {
+      const secret = generateSecret({
+        name: 'Text to show in Auth App', //display text in Google Auth App, you can put your domain and user email here
+        length: 16, //length of base32, this prop must be 8 multiple with limit at 32
+      });
+
+      //secret.base32 this prop used by check the user token input with logic in speakeasy
+
+      return `<a title=''>
+          <img src='https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=${secret.otpauth_url}' border=0>
+        </a>
+        <p>${secret.base32}</p>`;
+    } catch (err) {
+      console.error('ERRO', err);
+    }
+  }
+
+  @Post('validate')
+  async getQRCodeValidate(@Body() body): Promise<boolean> {
+    try {
+      const verify = totp.verify({
+        secret: body.secret,
+        encoding: 'base32',
+        token: body.token,
+      });
+
+      return verify;
+    } catch (err) {
+      console.error('ERRO', err);
+    }
+  }
+}
